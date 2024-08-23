1a5d7e64b8450426a6c4ccf23572ea5ec2cea86a
toshidapro <johnlowellsantos@gmail.com>
2024-08-23 16:49:48 +0000

    chore: task use cases (archive, rte on add task, wide title input, etc)
    Co-authored-by: Phippsy <michael@michaelphipps.com>

--- src/Models/Task/ListTasks.php
+++ src/Models/Task/ListTasks.php
@@ -8,12 +8,14 @@ class ListTasks
     public function __invoke()
     {
         $tasks = R::getAll(
-            'SELECT tasks.*, 
-                    users.name AS creator_name,
-                    staffs.name AS assigned_staff_name
+            'SELECT tasks.*,
+                users.name AS creator_name,
+                staffs.name AS assigned_staff_name
              FROM tasks
              LEFT JOIN users ON tasks.user_id = users.id
-             LEFT JOIN staffs ON tasks.assigned_to = staffs.id'
+             LEFT JOIN staffs ON tasks.assigned_to = staffs.id
+             WHERE tasks.archived = 0
+             ORDER BY tasks.created DESC'
         );
 
         return $tasks;
