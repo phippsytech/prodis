5c7397f2e0007d667fb908bd6fd410126d0c09e9
Michael Phipps <michael@michaelphipps.com>
2024-08-28 02:47:34 +0000

    Removed code that a cronjob will make unnecessary

--- check_service_agreement_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-while true
-do
-    php /var/www/prodis/src/Models/Participant/ServiceAgreement/WatchServiceAgreements.php produce
-    sleep 86400  # Sleep for 24 hours
-done
--- listen_service_agreement_date.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-while true
-do
-    php /var/www/prodis/src/Models/Participant/ServiceAgreement/ExpiredServiceAgreementWorker.php
-done
-
