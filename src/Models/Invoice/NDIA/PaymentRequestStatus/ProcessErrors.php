2be6f98f54216527eb71a3535ba255a92ac544c0
Michael Phipps <michael@michaelphipps.com>
2024-08-28 00:58:14 +0000

    Removed Xero from NDIA Error Processing

--- src/Models/Invoice/NDIA/PaymentRequestStatus/ProcessErrors.php
+++ src/Models/Invoice/NDIA/PaymentRequestStatus/ProcessErrors.php
@@ -6,116 +6,58 @@ use RedBeanPHP\R;
 
 class ProcessErrors
 {
-    private \NDISmate\Models\Invoice\NDIA\Remittance\Xero $xero;
-
-    public function __construct()
+    public function __invoke(): array
     {
-        $this->xero = new \NDISmate\Models\Invoice\NDIA\Remittance\Xero();
-    }
-
-    public function __invoke()
-    {
-        return ['http_code' => 400, 'error_message' => 'This is currently disabled on purpose.'];
-
-        $invoices = R::getAll(
-            "SELECT claim_reference
-             FROM ndiapaymentrequeststatuss
-             WHERE payment_request_status = 'ERROR'
-             AND claim_reference LIKE '%-310'
-             GROUP BY claim_reference"
+        $paymentErrors = R::getAll(
+            "SELECT pr.claim_reference, pr.id AS payment_request_id, tt.id AS timetracking_id
+            FROM ndiapaymentrequeststatuss pr
+            JOIN 
+                clients c ON c.ndis_number = pr.ndis_number
+            JOIN
+                timetrackings tt ON 
+                    tt.client_id = c.id
+                    AND tt.session_date = pr.supports_delivered_from
+                    AND COALESCE(NULLIF(tt.claim_type, ''), '') = COALESCE(NULLIF(pr.claim_type, ''), '')
+                    AND COALESCE(NULLIF(tt.cancellation_reason, ''), '') = COALESCE(NULLIF(pr.cancellation_reason, ''), '')
+                    AND tt.session_duration = pr.quantity
+                    AND tt.rate = pr.unit_price
+            WHERE pr.payment_request_status = 'ERROR'
+                AND pr.claim_reference LIKE '%-300'"
         );
 
-        // SELECT claim_reference
-        //      FROM ndiapaymentrequeststatuss
-        //      WHERE claim_reference = '430688725-280';
+        $billableIds = [];
+        $rejectedLineItems = []; 
 
-        $invoiceNumbers = $this->createXeroInvoiceNumberArray($invoices);
-
-        $result = $this->xero->accountingApi->getInvoices(
-            xero_tenant_id: $this->xero->tenant_id,
-            invoice_numbers: join(',', $invoiceNumbers)
-        );
-
-        $xeroInvoices = $result->getInvoices() ?? [];
-
-        $stuff = \XeroAPI\XeroPHP\AccountingObjectSerializer::sanitizeForSerialization($xeroInvoices);
-
-        // return ['http_code' => 200, 'result' => $stuff];
-
-        $filteredInvoiceNumbers = $this->getFilteredInvoiceNumbers($stuff, $invoiceNumbers);
-
-        foreach ($filteredInvoiceNumbers as $invoiceNumber) {
-            $ndismateInvoiceLineItems = (new \NDISmate\Models\Invoice\GetInvoice)(['invoice_number' => $invoiceNumber]);
-
-            if (empty($ndismateInvoiceLineItems)) {
-                continue;
-            }
-
-            if (empty($ndismateInvoiceLineItems['result'])) {
-                continue;
-            }
-            $ndismateInvoiceLineItems = $ndismateInvoiceLineItems['result'];
-
-            $xeroInvoice = $this->getXeroInvoiceByInvoiceNumber($xeroInvoices, $invoiceNumber);
-
-            $rejectedLineItems = [];
-            $billableIds = [];
-            foreach ($ndismateInvoiceLineItems as $lineItem) {
-                if (count($lineItem['errors']) > 0) {
-                    $rejectedLineItems[] = $lineItem;
-                    $billableIds = array_unique(array_merge($billableIds, $lineItem['SessionIds']));
-                }
-            }
-
-            $allItemsHaveErrors = count($ndismateInvoiceLineItems) === count($rejectedLineItems);
-
-            if (!empty($xeroInvoice)) {
-                if ($xeroInvoice->getAmountPaid() > 0) {
-                    // Invoice has a payment; cannot reverse it.
-                } else {
-                    if ($allItemsHaveErrors) {
-                        $xeroInvoice->setInvoiceNumber($xeroInvoice->getInvoiceNumber() . 'VOID');
-                        $xeroInvoice->setStatus('VOIDED');
-                    } else {
-                        [$xeroInvoice, $billableIds] = (new \NDISmate\Models\Invoice\RemoveXeroInvoiceLineItems)($xeroInvoice, $rejectedLineItems);
-                    }
-                    try {
-                        $this->updateXeroInvoice($xeroInvoice);
-                    } catch (\XeroAPI\XeroPHP\ApiException $e) {
-                        // Handle exception or log error
-                        return ['http_code' => 400, $e];
-                    }
-                }
-            }
-
-            $this->storeErrors($rejectedLineItems);
-            $this->reverseBillables($billableIds);
+        foreach ($paymentErrors as $paymentError) {
+            $this->storeError($paymentError);
+            $billableIds[] = $paymentError['timetracking_id'];
         }
 
-        return ['http_code' => 201, [$rejectedLineItems, $billableIds]];
+        $billableIds = array_unique($billableIds);
+        return ['http_code' => 201, 'data' => $billableIds];
+
+        $this->reverseBillables($billableIds);
+
+        return ['http_code' => 201, 'data' => [$rejectedLineItems, $billableIds]];
     }
 
     /**
-     * Store errors for line items.
+     * Store the errors for line items.
      *
-     * @param array $lineItems
+     * @param array $lineItem
      * @return void
      */
-    private function storeErrors(array $lineItems): void
+    private function storeError(array $lineItem): void
     {
-        foreach ($lineItems as $lineItem) {
-            foreach ($lineItem['SessionIds'] as $sessionId) {
-                $bean = R::findOrCreate(
-                    'billablendiaerrors',
-                    [
-                        'timetracking_id' => $sessionId,
-                        'payment_request_id' => $lineItem['error']
-                    ]
-                );
+        $bean = R::findOrCreate(
+            'billablendiaerrors',
+            [
+                'timetracking_id' => $lineItem['timetracking_id'],
+                'payment_request_id' => $lineItem['payment_request_id']
+            ]
+        );
 
-                R::store($bean);
-            }
-        }
+        R::store($bean);
     }
 
     /**
@@ -133,98 +75,5 @@ class ProcessErrors
         $placeholders = implode(',', array_fill(0, count($ids), '?'));
         $sql = "UPDATE timetrackings SET invoice_batch=null WHERE id IN ($placeholders)";
         R::exec($sql, $ids);
-    }
-
-    /**
-     * Find a Xero invoice by its invoice number.
-     *
-     * @param array $xeroInvoices
-     * @param string $invoiceNumber
-     * @param bool $triedReplacing
-     * @return mixed
-     */
-    private function getXeroInvoiceByInvoiceNumber(array $xeroInvoices, string $invoiceNumber, bool $triedReplacing = false)
-    {
-        foreach ($xeroInvoices as $invoice) {
-            if ($invoice->getInvoiceNumber() === $invoiceNumber) {
-                return $invoice;
-            }
-        }
-
-        if (!$triedReplacing) {
-            $modifiedInvoiceNumber = strpos($invoiceNumber, '-45-') !== false
-                ? str_replace('-45-', '-', $invoiceNumber)
-                : str_replace('-', '-45-', $invoiceNumber);
-
-            return $this->getXeroInvoiceByInvoiceNumber($xeroInvoices, $modifiedInvoiceNumber, true);
-        }
-
-        return null;
-    }
-
-    /**
-     * Updates a Xero invoice.
-     *
-     * @param mixed $invoice
-     * @return void
-     */
-    private function updateXeroInvoice($invoice)
-    {
-        try {
-            $this->xero->accountingApi->updateInvoice(
-                xero_tenant_id: $this->xero->tenant_id,
-                invoice_id: $invoice->getInvoiceID(),
-                invoices: $invoice
-            );
-        } catch (\XeroAPI\XeroPHP\ApiException $e) {
-            // Handle exception or log error
-        }
-    }
-
-    /**
-     * Create an array of Xero invoice numbers.
-     *
-     * Generates alternative invoice numbers for the old style numbering where
-     * we skipped -45- for NDIA invoices
-     *
-     * @param array $invoices
-     * @return array
-     */
-    private function createXeroInvoiceNumberArray(array $invoices): array
-    {
-        $processedInvoiceNumbers = [];
-
-        foreach ($invoices as $invoice) {
-            $invoiceNumber = $invoice['claim_reference'];
-            $processedInvoiceNumbers[] = $invoiceNumber;
-            $processedInvoiceNumbers[] = strpos($invoiceNumber, '-45-') !== false
-                ? str_replace('-45-', '-', $invoiceNumber)
-                : str_replace('-', '-45-', $invoiceNumber);
-        }
-
-        return array_unique($processedInvoiceNumbers);
-    }
-
-    /**
-     * Filters invoice numbers that appear in the invoices array.
-     *
-     * @param $invoices Array of invoices with 'InvoiceNumber' keys.
-     * @param array $invoiceNumbers Array of invoice numbers to filter.
-     * @return array Filtered list of invoice numbers that appear in the invoices array.
-     */
-    private function getFilteredInvoiceNumbers($invoices, array $invoiceNumbers): array
-    {
-        // Step 1: Extract the invoice numbers from the $invoices array
-        $extractedInvoiceNumbers = array_map(function ($invoice) {
-            return $invoice->InvoiceNumber;
-        }, $invoices);
-
-        // Step 2: Filter the $invoiceNumbers array
-        $filteredInvoiceNumbers = array_filter($invoiceNumbers, function ($invoiceNumber) use ($extractedInvoiceNumbers) {
-            return in_array($invoiceNumber, $extractedInvoiceNumbers);
-        });
-
-        // Reindex the array
-        return array_values($filteredInvoiceNumbers);
     }
 }
