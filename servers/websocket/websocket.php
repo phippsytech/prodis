71a422a06a30e78cebd491fcc236de9ab2089465
Michael Phipps <michael@michaelphipps.com>
2024-08-29 07:26:31 +0000

    removed commented code that is no longer needed

--- servers/websocket/websocket.php
+++ servers/websocket/websocket.php
@@ -9,12 +9,6 @@ require '/var/www/prodis/init.php';
     TODO: When we hit the concurrent connection limit, look at this article https://github.com/ratchetphp/Ratchet/issues/300
 */
 
-/**
- * NOTE: Port 8443 needs to be open on the server!  
- * sudo ufw allow 8443/tcp
- */
-
-
 $DOMAIN = WEBSOCKET_DOMAIN;
 $WEBSOCKET_PORT = 443;
 $unix_socket_file = '/tmp/ndismate.sock'; 
@@ -22,19 +16,8 @@ $unix_socket_file = '/tmp/ndismate.sock';
 ## WEB SOCKET
 $websocketServer = new \NDISmate\WebsocketServer\Server();
 
-
 // Sets up the websocket server
 $webSock = new \React\Socket\Server('0.0.0.0:'.$WEBSOCKET_PORT);
-
-// # This doesn't need ssl any more because that gets handled by traefik #
-// $webSock = new \React\Socket\SecureServer($webSock, null, [
-//     'local_cert' => '/etc/letsencrypt/live/'.$DOMAIN.'/fullchain.pem',
-//     'local_pk' => '/etc/letsencrypt/live/'.$DOMAIN.'/privkey.pem',
-//     'allow_self_signed' => FALSE,
-//     'verify_peer' => FALSE
-// ]);
-
-
 
 $webServer = new Ratchet\Server\IoServer(
     new Ratchet\Http\HttpServer(
@@ -66,10 +49,8 @@ pcntl_signal(SIGINT, function() use ($unix_socket, $unix_socket_file) {
 $unix_socket->on('connection', function (\React\Socket\ConnectionInterface $connection) use ($websocketServer) {
     $connection->on('data', function ($json) use ($connection, $websocketServer) {
 
-        //print($json);
         $data = json_decode($json, true);
 
-        //print_r($data);
         if ($data !== null) {
             $websocketServer->handleSocketData($json);
             $response = "OK";
