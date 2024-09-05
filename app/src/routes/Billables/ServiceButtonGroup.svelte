c03f8bfb363af69766fc3e55ac698dee293a59a8
toshidapro <johnlowellsantos@gmail.com>
2024-09-05 08:48:02 +0000

    chore: initial update for all services inactive

--- app/src/routes/Billables/ServiceButtonGroup.svelte
+++ app/src/routes/Billables/ServiceButtonGroup.svelte
@@ -31,7 +31,8 @@
             .then((result) => {
                 serviceList = []; // clear the servicList
                 services = result.result;
-
+                console.log(services);
+                
                 let selected = false;
 
                 services.forEach((service) => {
@@ -83,4 +84,30 @@
             {readOnly}
         />
     </div>
+{/if}
+
+{#if false}
+<div class="rounded-md bg-red-50 p-4">
+    <div class="flex">
+        <div class="flex-shrink-0">
+            <svg
+                class="h-5 w-5 text-red-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+            >
+                <path
+                    fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+        </div>
+        <div class="ml-3">
+            <h3 class="text-sm font-medium text-red-800">
+                This client's services are inactive.
+            </h3>
+        </div>
+    </div>
+</div>
 {/if}
