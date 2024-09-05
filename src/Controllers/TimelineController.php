11d38a76c8433607933c1a4d677d03bf99659f5c
Paolo Babasa <paolobabasa@gmail.com>
2024-09-05 04:43:20 +0000

    Fixes on the format return and optimization

--- src/Controllers/TimelineController.php
+++ src/Controllers/TimelineController.php
@@ -8,7 +8,7 @@ use NDISmate\Models\SIL\House\Timeline\GetTimeline;
 use NDISmate\Models\SIL\House\Timeline\DeleteTimeline;
 use NDISmate\Models\SIL\House\Timeline\GetTimelineList;
 use NDISmate\Models\SIL\House\Timeline\GetTimelineSummary;
-// use NDISmate\Models\SIL\House\Timeline\GetClientTimelineSummary;
+use NDISmate\Models\SIL\House\Timeline\GetClientTimelineSummary;
 
 
 final class TimelineController extends BaseController
@@ -20,8 +20,8 @@ final class TimelineController extends BaseController
             "showTimeline" => [new GetTimeline, null, false, [], []],
             'deleteTimeline' => [new DeleteTimeline, null, false, [], []],
             'getTimelineList' => [new GetTimelineList, null, false, []],
-            'getTimelineSummary' => [new GetTimelineSummary, null, false, [], []]
-            // 'getClientTimelineSummary' => [new GetClientTimelineSummary, null, true, []]
+            'getTimelineSummary' => [new GetTimelineSummary, null, false, [], []],
+            'getClientTimelineSummary' => [new GetClientTimelineSummary, null, false, []]
         ];
     }
 }
