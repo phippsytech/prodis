84e55fd58c336e44046e6a55b163caf0adc4c4a9
Michael Phipps <michael@michaelphipps.com>
2024-09-10 06:06:53 +0000

    Removed example implementation of StaffCapacityGraphy from SysAdmin

--- app/src/routes/SysAdmin/SysAdmin.svelte
+++ app/src/routes/SysAdmin/SysAdmin.svelte
@@ -17,9 +17,6 @@
     } from "@shared/stores.js";
     import { haveCommonElements } from "@shared/utilities.js";
 
-    //import StaffCapacityGraph.svelte
-    import StaffCapacityGraph from "./StaffCapacityGraph.svelte";
-
     export let roles;
     export let conditional = false;
 
@@ -59,34 +56,7 @@
     BreadcrumbStore.set({
         path: [{ url: null, name: "Crystel Care" }],
     });
-
-    //test data, inquire where to get the actual max capacity data from.
-    let labels = ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5'];
-
-    let datasets = [
-    {
-      label: 'Hours Worked',
-      data: [60, 50, 40, 30, 20], // Your custom data for Hours Worked
-      fill: true,
-      borderColor: 'rgba(75, 192, 192, 1)',
-      backgroundColor: 'rgba(75, 192, 192, 0.2)',
-      tension: 0.1, // Makes the line curved
-      order: 2 // Draw this line below the max capacity line
-    },
-    {
-      label: 'Max Capacity',
-      data: [30, 30, 30, 30, 30], // Your custom data for Max Capacity
-      borderColor: 'rgba(255, 99, 132, 1)', // Red color for the max capacity line
-      borderWidth: 2,
-      pointRadius: 0, // No points at the data points
-      fill: false, // Do not fill the area under this line
-      tension: 0, // Straight line
-      order: 1 // Draw this line above the actual data line
-    }
-  ];
 </script>
-
-<StaffCapacityGraph {labels} {datasets}/>
 
 {#if haveCommonElements($RolesStore, ["sysadmin"])}
     <!-- <NavBar hideMenu={true} /> -->
