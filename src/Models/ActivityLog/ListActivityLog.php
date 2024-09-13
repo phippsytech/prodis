eba129ea593dbad4ab07b5b7f1565f9327bbe690
toshidapro <johnlowellsantos@gmail.com>
2024-09-13 03:47:26 +0000

    chore: order by timeline descending

--- src/Models/ActivityLog/ListActivityLog.php
+++ src/Models/ActivityLog/ListActivityLog.php
@@ -14,23 +14,23 @@ class ListActivityLog
         if (isset($data['entity_type']) && isset($data['entity_id']) && isset($data['action_type'])) {
             $beans = R::findAll(
                 'activitylogs',
-                'entity_type = ? AND entity_id = ? AND action_type = ? ORDER BY timestamp',
+                'entity_type = ? AND entity_id = ? AND action_type = ? ORDER BY timestamp DESC',
                 [$data['entity_type'], $data['entity_id'], $data['action_type']]
             );
         } elseif (isset($data['entity_type']) && isset($data['entity_id'])) {
             $beans = R::findAll(
                 'activitylogs',
-                'entity_type = ? AND entity_id = ? ORDER BY timestamp',
+                'entity_type = ? AND entity_id = ? ORDER BY timestamp DESC',
                 [$data['entity_type'], $data['entity_id']]
             );
         } elseif (isset($data['action_type'])) {
             $beans = R::findAll(
                 'activitylogs',
-                'action_type = ? ORDER BY timestamp',
+                'action_type = ? ORDER BY timestamp DESC',
                 [$data['action_type']]
             );
         } else {
-            $beans = R::findAll('activitylogs', 'ORDER BY timestamp');
+            $beans = R::findAll('activitylogs', 'ORDER BY timestamp DESC');
         }
 
         // Convert beans to an array collection
