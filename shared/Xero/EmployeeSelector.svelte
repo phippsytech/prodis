16de2166e2631bbbbca26b52a2c16625bf15dbb7
Michael Phipps <michael@michaelphipps.com>
2024-09-23 11:32:52 +0000

    Updated to use FloatingCombo

--- shared/Xero/EmployeeSelector.svelte
+++ shared/Xero/EmployeeSelector.svelte
@@ -1,53 +1,29 @@
 <script>
-    // A drop down for selecting Xero Employee records isn't the right approach
-    // The right approach is for the CRM to create the Xero Employee record and then
-    // manage it from the CRM.  If there was a webhook received when the Xero Employee
+  import FloatingCombo from "@shared/PhippsyTech/svelte-ui/forms/FloatingCombo.svelte";
+  import { jspa } from "@shared/jspa.js";
 
-    // <XeroEmployeeSelector bind:xero_employee_id={employee.xero_employee_ref} />
+  export let xero_employee_id;
 
-    import FloatingSelect from "@shared/PhippsyTech/svelte-ui/forms/NewFloatingSelect.svelte";
-    import { jspa } from "@shared/jspa.js";
+  let readOnly = false;
 
-    export let xero_employee_id;
+  let items = [];
 
-    let readOnly = false;
-
-    let employees = [];
-    let employeeList = [];
-    let employeeSelectElement = null;
-
-    jspa("/Xero", "getEmployees", {})
-        .then((result) => {
-            employees = result.result;
-            employees.forEach((employee) => {
-                employeeList.push({
-                    option: employee.name,
-                    value: employee.id,
-                });
-            });
-
-            employeeList.sort(function (a, b) {
-                const nameA = a.option.toUpperCase(); // ignore upper and lowercase
-                const nameB = b.option.toUpperCase(); // ignore upper and lowercase
-                if (nameA < nameB) return -1;
-                if (nameA > nameB) return 1;
-                return 0; // names must be equal
-            });
-
-            employeeList = employeeList;
-        })
-        .catch(() => {});
-
-    function setXeroEmployeeId(xero_employee_id) {
-        xero_employee_id = xero_employee_id;
-    }
+  jspa("/Xero", "getEmployees", {})
+    .then((result) => {
+      items = result.result
+        .map((item) => ({
+          label: `${item.name}`,
+          value: item.id,
+        }))
+        .sort((a, b) => a.label.localeCompare(b.label));
+    })
+    .catch(() => {});
 </script>
 
-<FloatingSelect
-    on:change
-    bind:value={xero_employee_id}
-    label="Xero Employee Record"
-    instruction="Choose Xero Employee Record"
-    options={employeeList}
-    {readOnly}
+<FloatingCombo
+  label="Xero Employee Record"
+  {items}
+  bind:value={xero_employee_id}
+  placeholderText="Select or type employee name ..."
+  {readOnly}
 />
