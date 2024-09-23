13cba47bb94f47579887c1e982e0f573a1c8ec46
Paolo Babasa <paolobabasa@gmail.com>
2024-09-23 03:31:56 +0000

    remove <a>

--- app/src/routes/Accounts/GroupedLineItems.svelte
+++ app/src/routes/Accounts/GroupedLineItems.svelte
@@ -228,7 +228,7 @@
             {#if index == 0 || group[0].PlanManagerId != groupedItems[index - 1][0].PlanManagerId || group[0].NDISNumber != groupedItems[index - 1][0].NDISNumber}
                 <div class="py-0">
                     <span class="text-sm">
-                        <a>{group[0].ClientBillingName}</a>
+                        {group[0].ClientBillingName}
                     </span>
                 </div>
             {/if}
