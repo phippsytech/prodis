c406112ecd972f8e114a1a9cd6c6d6bc4e8bd9aa
Michael Phipps <michael@michaelphipps.com>
2024-09-23 11:14:03 +0000

    Using FloatingCombo to simplify other combo components.

--- app/src/routes/Service/ServiceCombo.svelte
+++ app/src/routes/Service/ServiceCombo.svelte
@@ -1,37 +1,25 @@
 <script>
-  import Select from "svelte-select";
+  import FloatingCombo from "@shared/PhippsyTech/svelte-ui/forms/FloatingCombo.svelte";
   import { jspa } from "@shared/jspa.js";
 
   export let value = null; // Default to null if no value is provided
 
-  let services = [];
   let items = [];
-  let placeholderText = "Select or type service ...";
 
   jspa("/Service", "listServices", {}).then((result) => {
-    // if (!isComponentMounted) return; // Exit if component is unmounted
-    services = result.result;
-    items = services.map((item) => ({
-      label: `${item.code}`,
-      value: item.id,
-    }));
-
-    // Sort the items
-    items.sort((a, b) => a.label.localeCompare(b.label));
+    // Map and sort in one step
+    items = result.result
+      .map((item) => ({
+        label: `${item.code}`,
+        value: item.id,
+      }))
+      .sort((a, b) => a.label.localeCompare(b.label));
   });
 </script>
 
-<div
-  class="rounded-md shadow-sm ring-1 ring-inset ring-indigo-100 bg-white mb-0 pb-1.5 mb-2"
->
-  <h3 class="px-3 pt-2 text-xs text-gray-500">Service</h3>
-
-  <Select
-    {items}
-    {value}
-    placeholder={placeholderText}
-    bind:justValue={value}
-    containerStyles="border:none; margin:0 0.75rem; padding:0 0; min-height:34px;width:auto;"
-    --list-position="fixed"
-  />
-</div>
+<FloatingCombo
+  label="Service"
+  {items}
+  bind:value
+  placeholderText="Select or type service ..."
+/>
