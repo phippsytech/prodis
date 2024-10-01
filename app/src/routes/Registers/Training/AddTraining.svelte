7bc8dab68f94cabbb9acdfb9f518eaccdcf5304b
Fel Pabustan <felpabustan93@gmail.com>
2024-10-01 06:36:40 +0000

    fix validation on AddTraining

--- app/src/routes/Registers/Training/AddTraining.svelte
+++ app/src/routes/Registers/Training/AddTraining.svelte
@@ -5,22 +5,14 @@
     import { toastSuccess, toastError } from "@shared/toastHelper.js";
     import FloatingInput from "@shared/PhippsyTech/svelte-ui/forms/FloatingInput.svelte";
     import FloatingDate from "@shared/PhippsyTech/svelte-ui/forms/FloatingDate.svelte";
-    import NewFloatingSelect from "@shared/PhippsyTech/svelte-ui/forms/NewFloatingSelect.svelte";
     import Button from "@shared/PhippsyTech/svelte-ui/Button.svelte";
     import StaffMultiSelector from "@shared/StaffMultiSelector.svelte";
-    import Role from "@shared/Role.svelte";
 
     let training = {};
 
     let evidenceOptions = [
-        {
-            option: "Yes",
-            value: "yes",
-        },
-        {
-            option: "No",
-            value: "no",
-        },
+        { option: "Yes", value: "yes" },
+        { option: "No", value: "no" },
     ];
 
     BreadcrumbStore.set({
@@ -31,46 +23,62 @@
         ]
     });
 
-    // update status based on completion date
+    // Update status based on completion date
     $: {
         const currentDate = new Date();
-        
+
         if (training.completion_date) {
             const completionDate = new Date(training.completion_date);
             training.status = completionDate <= currentDate ? "completed" : "in_progress"; 
         } else {
-            // If completion date is null, set status to "in_progress"
-            training.status = "in_progress";
+            training.status = "in_progress"; // Default status if no completion date
         }
     }
 
-    // $: if (training.date && training.completion_date && new Date(training.date) > new Date(training.completion_date)) {
-    //     toastError("The training start date should not be greater than the completion date.");
-    // }
+    const validations = [
+        { check: () => !training.staff_ids || training.staff_ids.length === 0, message: "Please select at least one staff member." },
+        { check: () => !training.course_title, message: "Course title must be provided." },
+        { check: () => !training.trainer, message: "Trainer must be provided." },
+        { check: () => !training.date, message: "Training start date must be provided." },
+    ];
+
+    function validate() {
+        for (const { check, message } of validations) {
+            if (check()) {
+                toastError(message);
+                return false;
+            }
+        }
+        return true;
+    }
 
     function addTraining() {
+        if (!validate()) {
+            return; 
+        }
         if (training.date) {
             if (training.completion_date) {
-                if (new Date(training.date) > new Date(training.completion_date)) {
-                    toastError("The training start date should not be greater than the completion date.");
-                    return; 
-                }
+                    if (new Date(training.date) > new Date(training.completion_date)) {
+                        toastError("The training start date should not be greater than the completion date.");
+                        return;
+                    }
             }
+
             jspa("/Register/Training", "addTraining", training)
                 .then(() => {
-                    push("/registers/trainings");
-                    toastSuccess("Training added successfully");
-                })
-                .catch(() => {
-                    toastError("Failed to add training");
-                });
+                push("/registers/trainings");
+                toastSuccess("Training added successfully");
+            })
+            .catch(() => {
+                toastError("Failed to add training");
+            });
         } else {
             toastError("Please enter a training start date.");
         }
     }
-
 </script>
-<StaffMultiSelector bind:staff_ids={training.staff_ids}/> 
+
+<StaffMultiSelector bind:staff_ids={training.staff_ids} /> 
 
 <FloatingInput
     bind:value={training.course_title}
@@ -113,5 +121,5 @@
 
 <div class="flex justify-between">
     <span></span>
-    <Button on:click={() => addTraining()} label="Add training" />
+    <Button on:click={addTraining} label="Add training" />
 </div>
