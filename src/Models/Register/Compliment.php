f89c216e35c1f0ba55c314933d08a81f21fd7e5d
Fel Pabustan <felpabustan93@gmail.com>
2024-10-06 13:47:42 +0000

    staffs_id and action_taken fields be optional

--- src/Models/Register/Compliment.php
+++ src/Models/Register/Compliment.php
@@ -15,8 +15,8 @@ class Compliment extends NewCustomModel
             'date' => [v::dateTime('Y-m-d')],
             'complimenter' => [v::stringType()],
             'description' => [v::stringType()],
-            'staffs_id' => [v::numericVal()],
-            'action_taken' => [v::stringType()],
+            'staffs_id' => [v::optional(v::numericVal())],
+            'action_taken' => [v::optional(v::stringType())],
             'acknowledgement_date' => [v::optional(v::dateTime('Y-m-d'))],
             'status' => [v::stringType()],
         ];
