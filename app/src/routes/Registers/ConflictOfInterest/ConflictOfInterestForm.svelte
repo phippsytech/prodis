90cce96883ee2a09753b479d29b06120caf0b36e
Paolo Babasa <paolobabasa@gmail.com>
2024-10-09 04:16:46 +0000

    wip form

--- app/src/routes/Registers/ConflictOfInterest/ConflictOfInterestForm.svelte
+++ app/src/routes/Registers/ConflictOfInterest/ConflictOfInterestForm.svelte
@@ -3,17 +3,26 @@
     import FloatingTextArea from "@shared/PhippsyTech/svelte-ui/forms/FloatingTextArea.svelte";
     import FloatingSelect from "@shared/PhippsyTech/svelte-ui/forms/FloatingSelect.svelte";
     import { jspa } from "@shared/jspa.js";
-
+    import FloatingDate from "@shared/PhippsyTech/svelte-ui/forms/FloatingDate.svelte";
+    import NewFloatingSelect from "@shared/PhippsyTech/svelte-ui/forms/NewFloatingSelect.svelte";
+    import RTE from "@shared/RTE/RTE.svelte";
+    
     export let conflictofinterest = {
-        status: "open",
+        status: "Unresolved",
     };
     export let readOnly = false;
 
     let conflictofinterestStatusSelectElement = null;
 
     let conflictofinterestStatusOptions = [
-        { option: "Open", value: "open" },
-        { option: "Closed", value: "closed" },
+        { option: "Resolved", value: "resolved" },
+        { option: "Ongoing", value: "ongoing" },
+        { option: "Unresolved", value: "unresolved" },
+    ];
+
+    let typeList = [
+        { option: "Organisational", value: "organisational" },
+        { option: "Individual", value: "individual" },
     ];
 
     let staff = [];
@@ -25,9 +34,11 @@
             staff = result.result;
             staff.forEach((staffer) => {
                 if (staffer.archived != 1)
-                    staffList.push({ option: staffer.name, value: staffer.id });
+                    staffList.push({ option: staffer.staff_name, value: staffer.id });
             });
             staffList = staffList;
+            console.log(staffList);
+            
         })
         .catch(() => {});
 
@@ -35,36 +46,49 @@
         readOnly = conflictofinterest.status == "closed";
     }
 </script>
+<div class="flex flex-wrap gap-2 items-center">
+    <div class="flex gap-2 flex-none">
+        <FloatingDate label="Conflict Date"  bind:value={conflictofinterest.conflict_date}/>
+        <FloatingSelect
+            bind:this={conflictofinterestStatusSelectElement}
+            bind:value={conflictofinterest.status}
+            label="Status"
+            instruction="Set Status"
+            options={conflictofinterestStatusOptions}
+            hideValidation={true}
+        />
+        <NewFloatingSelect 
+            on:change
+            bind:value={conflictofinterest.type}
+            label="Type"
+            instruction="Select type"
+            options={typeList}
+            {readOnly}
+            clearable>
+            
+            
+        </NewFloatingSelect>
 
-<FloatingSelect
-    bind:this={conflictofinterestStatusSelectElement}
-    bind:value={conflictofinterest.status}
-    label="Status"
-    instruction="Set Status"
-    options={conflictofinterestStatusOptions}
-    hideValidation={true}
-/>
+    </div>
+</div>
 
-<FloatingSelect
-    bind:this={staffSelectElement}
+
+<NewFloatingSelect 
+    on:change
     bind:value={conflictofinterest.staff_id}
     label="Who reported this conflict of interest"
     instruction="Choose staffer"
     options={staffList}
-    hideValidation={true}
     {readOnly}
-/>
+    clearable>
+</NewFloatingSelect>
 
-<FloatingTextArea
-    bind:value={conflictofinterest.description}
-    label="Description"
-    placeholder="Describe the conflict of interest"
-    style="height:150px"
-    {readOnly}
-/>
+
+
+<RTE bind:content={conflictofinterest.description} />
 <FloatingTextArea
     bind:value={conflictofinterest.resolution}
-    label="Resolution"
+    label="Actions Taken"
     placeholder="List actions taken to mitigate the conflict of interest."
     style="height:150px"
     {readOnly}
