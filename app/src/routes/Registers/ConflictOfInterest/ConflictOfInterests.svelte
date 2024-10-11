9ced21d1ff1e56e865d228cf9433fa4f46a52f3b
Paolo Babasa <paolobabasa@gmail.com>
2024-10-11 02:37:57 +0000

    filter wip

--- app/src/routes/Registers/ConflictOfInterest/ConflictOfInterests.svelte
+++ app/src/routes/Registers/ConflictOfInterest/ConflictOfInterests.svelte
@@ -7,7 +7,8 @@
     import { formatDate, formatPrettyName } from "@shared/utilities.js";
 
     let conflictofinterests = [];
-
+    let show_filter = false;
+    
     jspa("/Register/ConflictOfInterest", "listConflictOfInterests", {}).then(
         (result) => {
             conflictofinterests = result.result;
@@ -23,6 +24,10 @@
     BreadcrumbStore.set({ path: [{ url: "/registers", name: "Registers" },
     { url: "/registers/conflictofinterests", name: "Conflict of Interests" },
     ] });
+
+    function showFilter() {
+        show_filter = true;
+    }
 </script>
 
 <div class="sm:flex sm:items-center mb-4">
@@ -42,6 +47,31 @@
             >Add Conflict Of Interest</button
         >
     </div>
+</div>
+<div class="flex space-x-2 items-center">
+    <button
+      on:click={() => showFilter()}
+      class="w-8 h-8 relative inline-flex justify-center items-center rounded-lg hover:bg-indigo-600 hover:text-white"
+    >
+      <span class="sr-only">Filter</span>
+
+      <svg
+        data-slot="icon"
+        height="1em"
+        fill="none"
+        stroke-width="1.5"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
+        ></path>
+      </svg>
+    </button>
 </div>
 
 <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
