c1e8407b25ca8c58abeafcfdcab400a90f0afafd
Paolo Babasa <paolobabasa@gmail.com>
2024-10-11 02:26:41 +0000

    change the main page

--- app/src/routes/Registers/ConflictOfInterest/ConflictOfInterests.svelte
+++ app/src/routes/Registers/ConflictOfInterest/ConflictOfInterests.svelte
@@ -1,10 +1,10 @@
 <script>
     import { push } from "svelte-spa-router";
-
     import { PlusIcon } from "heroicons-svelte/24/outline";
     import { slide } from "svelte/transition";
     import { jspa } from "@shared/jspa.js";
     import { BreadcrumbStore } from "@shared/stores.js";
+    import { formatDate, formatPrettyName } from "@shared/utilities.js";
 
     let conflictofinterests = [];
 
@@ -20,7 +20,9 @@
         },
     );
 
-    BreadcrumbStore.set({ path: [{ url: "/registers", name: "Registers" }] });
+    BreadcrumbStore.set({ path: [{ url: "/registers", name: "Registers" },
+    { url: "/registers/conflictofinterests", name: "Conflict of Interests" },
+    ] });
 </script>
 
 <div class="sm:flex sm:items-center mb-4">
@@ -42,114 +44,68 @@
     </div>
 </div>
 
-<h1 class="text-black text-1xl font-bold mt-0 mb-2 drop-shadow mb-2">
-    Unresolved Conflict of Interests
-</h1>
-<ul class="bg-white rounded-lg border border-gray-200 w-full text-gray-900">
-    {#each conflictofinterests as conflictofinterest, index (conflictofinterest.id)}
-        {#if conflictofinterest.status == "unresolved"}
-            <li
-                in:slide={{ duration: 200 }}
-                out:slide|local={{ duration: 200 }}
-                on:click={() =>
-                    push(
-                        "/registers/conflictofinterests/" +
-                            conflictofinterest.id,
-                    )}
-                class="px-4 py-2 focus:outline-none focus:ring-0 focus:bg-gray-200 focus:text-gray-600 transition duration-500 cursor-pointer {conflictofinterest.length -
-                    1 ==
-                index
-                    ? 'rounded-b-lg'
-                    : ''}border-b border-gray-200 w-full {conflictofinterest.archived ==
-                1
-                    ? 'text-gray-400 cursor-default'
-                    : ''}"
-            >
-                <div class="list-group-item">
-                    <div class="justify-between flex">
-                        <span class="text-xs"
-                            >{conflictofinterest.date_identified}</span
-                        >
-                        <span class="text-xs">#{conflictofinterest.id}</span>
-                    </div>
-
-                    <div>
-                        {@html conflictofinterest.description.replace(
-                            /\n/g,
-                            "<br />",
-                        )}
-                    </div>
-                </div>
-            </li>
-        {/if}
-    {/each}
-</ul>
-
-<h1 class="text-black text-1xl font-bold mt-0 mb-2 drop-shadow mb-2">
-    Resolved Conflict of Interests
-</h1>
-<ul class="bg-white rounded-lg border border-gray-200 w-full text-gray-900">
-    {#each conflictofinterests as conflictofinterest, index (conflictofinterest.id)}
-        {#if conflictofinterest.status == "resolved"}
-            <li
-                in:slide={{ duration: 200 }}
-                out:slide|local={{ duration: 200 }}
-                on:click={() =>
-                    push(
-                        "/registers/conflictofinterests/" +
-                            conflictofinterest.id,
-                    )}
-                class="px-4 py-2 focus:outline-none focus:ring-0 focus:bg-gray-200 focus:text-gray-600 transition duration-500 cursor-pointer {conflictofinterest.length -
-                    1 ==
-                index
-                    ? 'rounded-b-lg'
-                    : ''}border-b border-gray-200 w-full {conflictofinterest.archived ==
-                1
-                    ? 'text-gray-400 cursor-default'
-                    : ''}"
-            >
-                <div class="list-group-item">
-                    <div class="justify-between flex">
-                        <span class="text-xs"
-                            >{conflictofinterest.date_identified}</span
-                        >
-                        <span class="text-xs">#{conflictofinterest.id}</span>
-                    </div>
-                    <div>
-                        {@html conflictofinterest.resolution.replace(
-                            /\n/g,
-                            "<br />",
-                        )}
-                    </div>
-                </div>
-            </li>
-        {/if}
-    {/each}
-</ul>
-
-<style>
-    .list-group-item {
-        max-height: 50px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        content: "";
-        position: relative;
-        transition: max-height 0.1s ease;
-    }
-    .list-group-item:before {
-        content: "";
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        left: 0;
-        top: 0;
-        background: linear-gradient(transparent 25px, white);
-    }
-
-    .list-group-item.expanded {
-        max-height: 10000px;
-    }
-    .list-group-item.expanded:before {
-        background: none;
-    }
-</style>
+<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+        <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-300">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="py-2 px-4 text-left text-xs font-medium text-slate-500">
+                            Conflicts
+                        </th>
+                        <th scope="col" class="py-2 px-4 text-left text-xs font-medium text-slate-500">
+                            Parties Involved 
+                        </th>
+                        <th scope="col" class="py-2 px-4 text-left text-xs font-medium text-slate-500">
+                            Implementing Staff
+                        </th>
+                        <th scope="col" class="py-2 px-4 text-left text-xs font-medium text-slate-500">
+                            Conflict Date
+                        </th>
+                        <th scope="col" class="py-2 px-4 text-left text-xs font-medium text-slate-500">
+                            Resolution Date
+                        </th>
+                        <th scope="col" class="py-2 px-4 text-left text-xs font-medium text-slate-500">
+                            Status
+                        </th>
+                        <th scope="col" class="relative py-3.5 pl-3 pr-4 xs:pr-6">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 bg-white">
+                    {#each conflictofinterests as conflict}
+                        <tr>
+                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                {conflict.description.length > 50 
+                                    ? conflict.description.slice(0, 50) + '...' 
+                                    : conflict.description}
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                {conflict.involved_staff_name}
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                {conflict.parties_involved ? conflict.parties_involved : 'N/A'}
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                {formatDate(conflict.date_identified)}
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                {formatDate(conflict.date_resolved) ? formatDate(conflict.date_resolved) : 'N/A'}
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                {formatPrettyName(conflict.status)}
+                            </td>
+                            <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                <a href="/#/registers/conflictofinterests/{conflict.id}" class="text-indigo-600 hover:text-indigo-900">
+                                    Edit
+                                    <span class="sr-only">, {conflict.resolution}</span>
+                                </a>
+                            </td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
