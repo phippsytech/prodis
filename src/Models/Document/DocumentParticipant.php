1155064ef5817173bfe89e2b91900317ec4a0b53
Paolo Babasa <paolobabasa@gmail.com>
2024-10-16 00:30:20 +0000

    Fixes on update participants on document

--- src/Models/Document/DocumentParticipant.php
+++ src/Models/Document/DocumentParticipant.php
@@ -21,15 +21,40 @@ class DocumentParticipant extends NewCustomModel
     public function createDocumentParticipant($data) {
         
         $partcipantIds = $data['participant_ids'];
+        $params = ['document_id' => $data['document_id']];
+
+        // Fetch existing records for the given document_id
+        $existingRec = R::getAll('SELECT participant_id FROM documentparticipants WHERE document_id = :document_id', $params);
+
+        // Get the list of participant_ids that need to be deleted
+        $existingParticipantIds = array_column($existingRec, 'participant_id');
+        $toDelete = array_diff($existingParticipantIds, $data['participant_ids']);
+
+        // 1. Delete participants that are not in the new list
+        if ($toDelete) {
+            foreach ($toDelete as $participantId) {
+                $record = R::findOne('documentparticipants', 'document_id = ? AND participant_id = ?', [$data['document_id'], $participantId]);
+                if ($record) {
+                    R::trash($record); 
+                }
+            }
+        }
+
+        // 2. Add new participants that don't already exist
+        $toAdd = array_diff($data['participant_ids'], $existingParticipantIds);
         $created = [];
-        foreach ($partcipantIds as $id) {
-           $created[] =  $this->create([
+        foreach ($toAdd as $id) {
+        $created[] = $this->create([
                 'participant_id' => $id,
                 'document_id' => $data['document_id']
             ]);
         }
 
-        return $created;
+        // Fetch and return the updated list of participant_ids
+        $result = R::getAll('SELECT participant_id FROM documentparticipants WHERE document_id = :document_id', $params);
+
+        return array_column($result, 'participant_id');
+
 
     }
 
